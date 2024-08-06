d2c2625126537520fbd4931b5e5833ebba1d5b3d
DylanRothbauer <djrothbauer@gmail.com>
2024-08-05 20:40:30 -0500

    LeetCode 75/206

---
+++ Leetcode/LeetCode 75/206. Reverse Linked List/main.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        ListNode* next = nullptr;
+
+        while (curr != nullptr) {
+            // set next
+            next = curr->next;
+
+            // point curr to prev
+            curr->next = prev;
+
+            // move pointers
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+};
